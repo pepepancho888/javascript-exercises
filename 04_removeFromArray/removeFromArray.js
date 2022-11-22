44490bfb049136915a8bad391cef4cac7ad3fa44
pepepancho888 <pepepanchocepillo@gmail.com>
2022-11-22 15:34:32 -0500

    Solve Exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray,...itemsToRemove) {
+    let position ='';
+    for(let j=0;j<itemsToRemove.length;j++) {
+        for(let i=0;i<originalArray.length;i++) {
+            if (itemsToRemove[j] === originalArray[i]) {
+                position = i;
+                let beforeValues = originalArray.slice(0,position);
+                let afterValues = originalArray.slice(position + 1);
+                originalArray = beforeValues.concat(afterValues);
+            }
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
