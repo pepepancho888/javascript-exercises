71dc09a8cb49314f2d2ff4a945a463fb39936fce
pepepancho888 <pepepanchocepillo@gmail.com>
2022-11-22 00:05:39 -0500

    Solve Exercise 03 reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myStringArray = string.split("");
+    myStringArray.reverse();
+    let myStringReversed = myStringArray.join("");
+    return myStringReversed;
 };
+
+/*
+const reverseString = function(string) {
+    let myArray = string.split("");
+    let myArrayReversed = [];
+    for (let i = myArray.length - 1; i >= 0; i--)
+    {
+        myArrayReversed.push(myArray[i]);
+    }
+    console.log(myArrayReversed);
+    let myStringReversed = myArrayReversed.join("");
+    return myStringReversed;
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
