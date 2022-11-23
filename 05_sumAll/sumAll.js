f8aa0408054cc72d349afb7be5d35e607adf1bfd
pepepancho888 <pepepanchocepillo@gmail.com>
2022-11-22 20:10:08 -0500

    Solve Exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let mayor = 0;
+    let menor = 0;
+    if (num1 < 0 || num2 < 0 || typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    } else {
+        if (num1>num2){
+            mayor = num1;
+            menor = num2;
+        } else {
+            mayor = num2;
+            menor = num1;
+        }
+        for (i=menor;i<=mayor;i++) {
+            sum +=i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
